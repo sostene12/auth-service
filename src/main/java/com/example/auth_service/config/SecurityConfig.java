0dcb961d28ee4ac00ad5d03c8424d698ba8a6b2c
Ngarukiyimana Sostene <ngarukiyimanasostene@gmail.com>
2025-05-03 23:07:42 +0200

    cor config

--- src/main/java/com/example/auth_service/config/SecurityConfig.java
+++ src/main/java/com/example/auth_service/config/SecurityConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -14,7 +12,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        http.
+                cors().
+                and()
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
--- src/main/java/com/example/auth_service/config/WebConfig.java
+++ src/main/java/com/example/auth_service/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173", "https://auth-service-production-7c6c.up.railway.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowCredentials(true);
             }
